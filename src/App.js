d322677852411d39f3ec60df9a522805e0782c0a
stere8 <twizoraph4247@gmail.com>
2024-05-26 09:29:37 +0200

    keep em hoinh

--- src/App.js
+++ src/App.js
@@ -2,35 +2,56 @@ import logo from "./logo.svg";
 import FC from "react";
 import "./App.css";
 import ImageCard from "./components/ImageCard";
+import AngeImage from "./src/img/Ange.jpg";
+
 
 function App() {
   return (
     <div className="App">
       <div class="container">
-        <ImageCard 
-        message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
-        am today. Thank you for being the constant in my ever-changing world. " 
-        image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
-        name="asd"
-        left />
-        <ImageCard 
-        message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
-        am today. Thank you for being the constant in my ever-changing world. " 
-        image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
-        name="asd"
-        left = {false} />
-        <ImageCard 
-        message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
-        am today. Thank you for being the constant in my ever-changing world. " 
-        image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
-        name="asd"
-        left />
-        <ImageCard 
-        message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
-        am today. Thank you for being the constant in my ever-changing world. " 
-        image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
-        name="asd"
-        left = {false} />
+        <ImageCard
+          message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
+        am today. Thank you for being the constant in my ever-changing world. "
+          image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+          name="asd"
+          left
+        />
+        <ImageCard
+          message="Happy mothers day. all we are today is thanks to you and we will always do our best to make you proud and happy. "
+          image={AngeImage}
+          name="~Ange"
+          left={false}
+        />
+        <ImageCard
+          message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
+        am today. Thank you for being the constant in my ever-changing world. "
+          image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+          name="asd"
+          left
+        />
+        <ImageCard
+          message="Mom, as I reflect on my life, I realize the countless ways you've shaped me. Your unwavering support through thick and thin, your gentle guidance during tough decisions, and your infectious laughter during joyful moments – all have woven a tapestry of who I
+        am today. Thank you for being the constant in my ever-changing world. "
+          image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+          name="asd"
+          left={false}
+        />
+      </div>
+      <div>
+        <iframe
+          width="300"
+          height="60"
+          src="https://vocaroo.com/embed/151i8tomI0XS?autoplay=1"
+          frameborder="0"
+          allow="autoplay"
+        ></iframe>
+        <a
+          href="https://voca.ro/151i8tomI0XS"
+          title="Mama Mwiza"
+          target="_blank"
+        >
+          View on Vocaroo &gt;&gt;
+        </a>
       </div>
     </div>
   );
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,7 +5,6 @@ const ImageCard = ({ name, message, image, left = true }) => {
     <div className="row align-items-start big-container">
       {left ? (
         <>
-          <div className="col-md-8 d-flex align-items-center content-container">
             <div className="col-md-4">
               <img
                 className="img-fluid rounded-circle"
@@ -13,6 +12,7 @@ const ImageCard = ({ name, message, image, left = true }) => {
                 alt="Image"
               />
             </div>
+            <div className="col-md-8 d-flex align-items-center content-container">
             <div className="text-container">
               <p>{message}</p>
               <span className="author">{name}</span>
